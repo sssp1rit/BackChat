fadc65c6c28f103ccd50cbc9be464d75598ca511
evgn <aosaev3415@gmail.com>
2025-06-17 18:36:06 +0300

    ячсячс

--- routes/message.js
+++ routes/message.js
@@ -2,41 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Message = require('../models/Message');
 const mongoose = require('mongoose');
-const { ObjectId } = require('mongodb');
-
-// Получить историю сообщений между двумя пользователями
-router.get('/:userId', async (req, res) => {
-  const currentUserId = req.query.currentUserId;
-  const otherUserId = req.params.userId;
-
-  try {
-    const messages = await Message.find({
-      $or: [
-        { from: currentUserId, to: otherUserId },
-        { from: otherUserId, to: currentUserId }
-      ]
-    }).sort({ timestamp: 1 });
-
-    res.json(messages.map(m => ({
-      text: m.text,
-      from: m.from.toString()
-    })));
-  } catch (err) {
-    res.status(500).json({ error: 'Ошибка при получении сообщений' });
-  }
-});
-
-// Отправка сообщения
-router.post('/', async (req, res) => {
-  const { from, to, text } = req.body;
-
-  try {
-    const message = await Message.create({ from, to, text });
-    res.status(201).json({ success: true, message });
-  } catch (err) {
-    res.status(500).json({ error: 'Ошибка при отправке сообщения' });
-  }
-});
 
 // Получить количество непрочитанных сообщений, сгруппированных по отправителю
 router.get('/unreadCounts/:userId', async (req, res) => {
@@ -78,5 +43,43 @@ router.post('/markAsRead', async (req, res) => {
     res.status(500).json({ message: 'Ошибка при пометке сообщений как прочитанных' });
   }
 });
+
+// Получить историю сообщений между двумя пользователями
+router.get('/:userId', async (req, res) => {
+  const currentUserId = req.query.currentUserId;
+  const otherUserId = req.params.userId;
+
+  try {
+    const messages = await Message.find({
+      $or: [
+        { from: currentUserId, to: otherUserId },
+        { from: otherUserId, to: currentUserId }
+      ]
+    }).sort({ timestamp: 1 });
+
+    res.json(messages.map(m => ({
+      text: m.text,
+      from: m.from.toString()
+    })));
+  } catch (err) {
+    res.status(500).json({ error: 'Ошибка при получении сообщений' });
+  }
+});
+
+// Отправка сообщения
+router.post('/', async (req, res) => {
+  const { from, to, text } = req.body;
+
+  try {
+    const message = await Message.create({ from, to, text });
+    res.status(201).json({ success: true, message });
+  } catch (err) {
+    res.status(500).json({ error: 'Ошибка при отправке сообщения' });
+  }
+});
+
+
+
+
 
 module.exports = router;
