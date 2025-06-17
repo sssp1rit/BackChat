cc5bf0cd497af4634bc0bbb1dd9428ddbeefa938
evgn <aosaev3415@gmail.com>
2025-06-17 18:25:28 +0300

    трашвт

--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Message = require('../models/Message');
 const mongoose = require('mongoose');
+const { ObjectId } = require('mongodb');
 
 // Получить историю сообщений между двумя пользователями
 router.get('/:userId', async (req, res) => {
